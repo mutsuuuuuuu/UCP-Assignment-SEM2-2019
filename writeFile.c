07d7f7a535f268a96531e5747721df0df97c866a
f_fuuuuuuuuuuuuccc ツ <47999921+mutsuuuuuuu@users.noreply.github.com>
2019-11-15 09:53:09 +0800

    Update writeFile.c

--- writeFile.c
+++ writeFile.c
@@ -1,6 +1,5 @@
 /**
 FILENAME: writeFile.c
-CREATED BY:DICKY LARSON GULTOM 19487537
 PURPOSE: printing all of the contents inside the linked list to a file.
 GITHUB REPOSITORY : https://github.com/mutsuuuuuuu/UCP-Assignment-SEM2-2019
 **/
